bedef6b9868232ba4eba0f69744d0dbf2f686ebc
Little-Make-Stick <1911006352@qq.com>
2022-06-01 17:02:42 +0800

    [UPDATE]首页菜单数据与页面代码分离

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,58 +1,106 @@
 <template>
-  <div class="home">
-    <el-container style="height: 100vh; border: 1px solid #eee;">
-      <el-aside width="200px" style="background-color: rgb(238, 241, 246);text-align: left;">
-        <el-menu  router :default-active="$route.path" :collapse="isCollapse">
-          <el-submenu index="1">
-            <template slot="title" ><i class="el-icon-message"></i>Bar Chart</template>
-            <el-menu-item index="/barchart/base">Basic</el-menu-item>
-          </el-submenu>
-          <el-submenu index="2">
-            <template slot="title" ><i class="el-icon-message"></i>Line Chart</template>
-            <el-menu-item index="/linechart/base">Basic</el-menu-item>
-          </el-submenu>
-          <el-submenu index="3">
-            <template slot="title" ><i class="el-icon-message"></i>Pie Chart</template>
-            <el-menu-item index="/piechart/base">Basic</el-menu-item>
-          </el-submenu>
-          <el-submenu index="4">
-            <template slot="title" ><i class="el-icon-message"></i>Scatter Chart</template>
-            <el-menu-item index="/scatterchart/base">Basic</el-menu-item>
-          </el-submenu>
-        </el-menu>
-      </el-aside>
+    <div class="home">
+        <el-container style="height: 100vh; border: 1px solid #eee">
+            <!-- 侧边栏 -->
+            <el-aside width="200px" style="background-color: rgb(238, 241, 246); text-align: left">
+                <el-menu router :default-active="$route.path" :collapse="isCollapse">
+                    <el-submenu v-for="subMenu in menuList" :key="subMenu.index" :index="subMenu.index">
+                        <template slot="title">
+                            <i :class="subMenu.icon"></i>
+                            {{ subMenu.title }}
+                        </template>
+                        <el-menu-item v-for="menuItem in subMenu.children" :key="menuItem.index" :index="menuItem.index">
+                          {{ menuItem.title }}
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
 
-      <el-container>
-        <el-header style="text-align: right; font-size: 12px;background: rgb(238, 241, 246);display: flex;align-items: center;justify-content: end;">
-          <el-dropdown>
-            <i class="el-icon-setting" style="margin-right: 15px"></i>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>查看</el-dropdown-item>
-              <el-dropdown-item>新增</el-dropdown-item>
-              <el-dropdown-item>删除</el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-          <span>xxx</span>
-        </el-header>
-
-        <el-main>
-            <router-view :key="$route.path"></router-view>
-        </el-main>
-      </el-container>
-    </el-container>
-  </div>
+            <el-container>
+                <!-- 头部 -->
+                <el-header
+                    style="
+                        text-align: right;
+                        font-size: 12px;
+                        background: rgb(238, 241, 246);
+                        display: flex;
+                        align-items: center;
+                        justify-content: end;
+                    "
+                >
+                    <el-dropdown>
+                        <i class="el-icon-setting" style="margin-right: 15px"></i>
+                        <el-dropdown-menu slot="dropdown">
+                            <el-dropdown-item>查看</el-dropdown-item>
+                            <el-dropdown-item>新增</el-dropdown-item>
+                            <el-dropdown-item>删除</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                    <span>xxx</span>
+                </el-header>
+                <!-- 主要内容 -->
+                <el-main>
+                    <router-view :key="$route.path"></router-view>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
-
 export default {
-  name: "Home",
-  components: {
-  },
-  data(){
-    return{
-      isCollapse: false,
-    }
-  }
-};
+    name: 'Home',
+    components: {},
+    data() {
+        return {
+            isCollapse: false,
+            menuList: [
+                {
+                    index: '1',
+                    icon: '',
+                    title: '柱状图',
+                    children: [
+                        {
+                            index: '/barchart/base',
+                            title: '基础语法',
+                        },
+                    ],
+                },
+                {
+                    index: '2',
+                    icon: '',
+                    title: '折线图',
+                    children: [
+                        {
+                            index: '/linechart/base',
+                            title: '基础语法',
+                        },
+                    ],
+                },
+                {
+                    index: '3',
+                    icon: '',
+                    title: '饼图',
+                    children: [
+                        {
+                            index: '/piechart/base',
+                            title: '基础语法',
+                        },
+                    ],
+                },
+                {
+                    index: '4',
+                    icon: '',
+                    title: '散点图',
+                    children: [
+                        {
+                            index: '/scatterchart/base',
+                            title: '基础语法',
+                        },
+                    ],
+                },
+            ],
+        }
+    },
+}
 </script>
